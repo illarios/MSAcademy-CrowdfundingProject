96abd620eacb4342255a514f396e31eb88e2905c
Triantafyllos Samoladas <triasamo1@ee.duth.gr>
2020-11-19 19:18:14 +0200

    Add Image and Tag Class

---
+++ CrowdFundingProject/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrowdFundingProject.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string ImageTitle { get; set; }
+        public byte[] ImageData { get; set; }
+    }
+}
---
+++ CrowdFundingProject/Models/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrowdFundingProject.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
