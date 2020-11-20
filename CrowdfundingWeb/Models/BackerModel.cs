68469cde1b937220be48d0d704afddf17af43618
rotask <rotaskost@gmail.com>
2020-11-20 19:09:32 +0200

    BackerModel

---
+++ CrowdfundingWeb/Models/BackerModel.cs
@@ -0,0 +1,14 @@
+﻿using CrowdFundingProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrowdfundingWeb.Models
+{
+    public class BackerModel
+    {
+        public List<Backer> Backers { get; set; }
+        public Backer Backer { get; set; }
+    }
+}
