77be39a233c0f64425fed5b6e1c3e07aafaa5f6c
Triantafyllos Samoladas <triasamo1@ee.duth.gr>
2020-11-21 11:36:30 +0200

    Add JS functions for project and bundle processes

--- CrowdfundingWeb/wwwroot/js/site.js
+++ CrowdfundingWeb/wwwroot/js/site.js
@@ -1,4 +1,181 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+
+
+//====================== P R O J E C T S =======================
+
+function addProject() {
+    actionUrl = "/api/project/create"    //<------------
+    actiontype = "POST"
+    actionDataType = "json"
+
+    sendData = {
+        "title": $("#Title").val(),
+        "description": $("#Description").val(),
+        "category": $("#Category").val(),
+        "goal": $("#Goal").val(),
+        "endDate": $("#EndDate").val()
+    }
+
+    $.ajax({
+        url: actionUrl,
+        dataType: actionDataType,
+        type: actiontype,
+        data: JSON.stringify(sendData),
+        contentType: 'application/json',
+        processData: false,
+
+        success: function (data, textStatus, jQxhr) {
+            alert(JSON.stringify(data))
+            window.open("/home/customers", "_self")
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
+
+function updateProject() {
+    id = $("#Id").val()
+    actionUrl = "/api/project/update/"+id    //<----------
+    actiontype = "PUT"
+    actionDataType = "json"
+
+    sendData = {
+        "title": $("#Title").val(),
+        "description": $("#Description").val(),
+        "category": $("#Category").val(),
+        "goal": $("#Goal").val(),
+        "endDate": $("#EndDate").val()
+    }
+
+    $.ajax({
+        url: actionUrl,
+        dataType: actionDataType,
+        type: actiontype,
+        data: JSON.stringify(sendData),
+        contentType: 'application/json',
+        processData: false,
+
+        success: function (data, textStatus, jQxhr) {
+            alert(JSON.stringify(data))
+            window.open("/home/customers", "_self")
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
+
+function updateProject() {
+    id = $("#Id").val()
+
+    actionUrl = "/api/project/delete/" + id    //<----------
+    actiontype = "DELETE"
+    actionDataType = "json"
+
+    $.ajax({
+        url: actionUrl,
+        dataType: actionDataType,
+        type: actiontype,
+        contentType: 'application/json',
+        processData: false,
+
+        success: function (data, textStatus, jQxhr) {
+
+            alert(JSON.stringify(data))
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
+
+//====================== B U N D L E S =======================
+
+function addBundle() {
+    id = $("#Id").val()
+
+    actionUrl = "/api/project/"+ id +"/bundle/add"    //<------------
+    actiontype = "POST"
+    actionDataType = "json"
+
+    sendData = {
+        "description": $("#Description").val(),
+        "prize": $("#Prize").val(),
+        "goal": $("#Goal").val(),
+    }
+
+    $.ajax({
+        url: actionUrl,
+        dataType: actionDataType,
+        type: actiontype,
+        data: JSON.stringify(sendData),
+        contentType: 'application/json',
+        processData: false,
+
+        success: function (data, textStatus, jQxhr) {
+            alert(JSON.stringify(data))
+            window.open("/home/customers", "_self")
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
+
+function updateBundle() {
+    id = $("#Id").val()
+
+    actionUrl = "/api/project/" + id + "/bundle/update"    //<------------
+    actiontype = "PUT"
+    actionDataType = "json"
+
+    sendData = {
+        "description": $("#Description").val(),
+        "prize": $("#Prize").val(),
+        "goal": $("#Goal").val(),
+    }
+
+    $.ajax({
+        url: actionUrl,
+        dataType: actionDataType,
+        type: actiontype,
+        data: JSON.stringify(sendData),
+        contentType: 'application/json',
+        processData: false,
+
+        success: function (data, textStatus, jQxhr) {
+            alert(JSON.stringify(data))
+            window.open("/home/customers", "_self")
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
+
+function deleteBundle() {
+    id = $("#Id").val()
+
+    actionUrl = "/api/project/" + id + "/bundle/delete"    //<------------
+    actiontype = "DELETE"
+    actionDataType = "json"
+
+    $.ajax({
+        url: actionUrl,
+        dataType: actionDataType,
+        type: actiontype,
+        contentType: 'application/json',
+        processData: false,
+
+        success: function (data, textStatus, jQxhr) {
+
+            alert(JSON.stringify(data))
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
